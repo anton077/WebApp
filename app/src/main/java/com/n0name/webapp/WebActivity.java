6b0481dc8a60ec972811e93634496561dcee1d1a
anton077 <antoshapiro@gmail.com>
2016-02-06 17:32:05 +0200

    SecondCommitNewBranch

--- app/src/main/java/com/n0name/webapp/WebActivity.java
+++ app/src/main/java/com/n0name/webapp/WebActivity.java
@@ -16,7 +16,7 @@ public class WebActivity extends Activity {
         // String str = "<body><h2>boga goga</h2><h4>gogo mogo";
         // str += "lala</h4></body>";
 
-//change!
+//change!@@
 
         browser.setWebViewClient(new URLIntercepter());
         //   browser.loadData(str, "text/html", "UTF-8");
